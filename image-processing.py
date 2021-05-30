f1f6f0380c082b97f25851019acff90739b0eb32
Edip Çiftçi <ediler1970@gmail.com>
2021-05-30 20:40:23 +0300

    Model is trained

--- image-processing.py
+++ image-processing.py
@@ -17,3 +17,6 @@ test_images = test_images / 255.0
 
 model = keras.Sequential([keras.layers.Flatten(input_shape=(28, 28)), keras.layers.Dense(128, activation=tf.nn.relu),
                           keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=5)
