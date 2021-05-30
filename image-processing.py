e5ad1154aa4d0a369d4c8ad1404b47c78f4ec776
Edip Çiftçi <ediler1970@gmail.com>
2021-05-30 20:50:27 +0300

    Project finished

--- image-processing.py
+++ image-processing.py
@@ -10,7 +10,7 @@ plt.figure()
 plt.imshow(train_images[0])
 plt.colorbar()
 plt.grid(False)
-plt.show()
+# plt.show()
 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
@@ -20,3 +20,11 @@ model = keras.Sequential([keras.layers.Flatten(input_shape=(28, 28)), keras.laye
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(test_acc)
+
+predictions = model.predict(test_images)
+print(predictions[0])
+
+print(numpy.argmax(predictions[0]))
